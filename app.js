bc62039ba7af7540c0805bfdf1223ee4d3990971
Darksied95 <aliyuefe9501@gmail.com>
2022-11-29 22:08:47 +0100

    extracted forecast from app.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const request = require('request');
 const geocode = require('./utils/geocode')
-
-const url = 'http://api.weatherstack.com/current?access_key=9408cd64ecc47f8960b0391b0e503d7d&query=6.5244,3.3792'
-
+const forecast = require('./utils/forecast')
 
 
-request({ url, json: true }, (err, res) => {
-    if (err) {
-        console.log('Unable to connect to weather API')
-    } else if (res.body.error) {
-        console.log('Unable to find location');
-    }
-    else {
-        const { temperature, feelslike } = res.body.current
-        console.log(`It's currently ${temperature} degress outside but it feels like ${feelslike} degrees`);
-    }
+
+forecast(6.5244, 3.3792, (err, res) => {
+    console.log('Error', err);
+    console.log('Result', res);
 })
-
 
 
 geocode('Lagos', (err, res) => {
---
+++ utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+function forecast(lat, long, callback) {
+    const url = `http://api.weatherstack.com/current?access_key=9408cd64ecc47f8960b0391b0e503d7d&query=${lat},${long}`
+
+
+
+    request({ url, json: true }, (err, res) => {
+        if (err) {
+            callback('Unable to connect to weather API', undefined)
+        } else if (res.body.error) {
+            console.log(res.body.error);
+            callback('Unable to find location', undefined);
+        }
+        else {
+            const { temperature, feelslike } = res.body.current
+            callback(undefined, `It's currently ${temperature} degress outside but it feels like ${feelslike} degrees`);
+        }
+    })
+}
+
+module.exports = forecast
